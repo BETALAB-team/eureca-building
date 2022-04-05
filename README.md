c29051e39ce4ad818df10d60fb47ef61be2ad10c
Prataviera Enrico <enrico.prataviera@studenti.unipd.it>
2022-04-05 10:52:27 -0700

    Creation of the package

---
+++ README.md
@@ -0,0 +1,11 @@
+# eureca-buildings
+## Dynamic building simulation  according to lumped capacitance models
+
+Small package to create and run the dynamic simulation of a building according to VDI6007 or ISO 13790
+
+==Take a look to example file to see some example on how to use it.==
+Requires at leats pandas and numpy, but if you want use conda to install a environment with everything.
+
+```
+conda -env create -f conda_env_requirements.yml
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8'
+]
+
+setup(
+   name='eureca-buildings',
+   version='0.1.0',
+   author='betalab group UNIPD',
+   author_email='enrico.prataviera@phd.unipd.it',
+   packages=find_packages(),
+   scripts=[],
+   url='https://github.com/BETALAB-team/eureca-buildings',
+   license='LICENSE',
+   description='Package to simulate building energy performace with lumped capacitance models',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "numpy",
+       "pandas",
+       "pytest",
+       "matplotlib",
+   ],
+)
